6bdd9071e1088578790fab6956b1fa3ccd991cb3
antosox <antoine.burgaudeau@epsi.fr>
2018-02-22 15:42:00 +0100

    formulaire

--- formulaire_login.php
+++ formulaire_login.php
@@ -4,10 +4,11 @@
     <head>
         <meta charset="UTF-8">
         <link href="formulaire_login.css" rel="stylesheet" type="text/css"/>
-        
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
     </head>
     <body>
+        <?php include "headbootstrap.php" ?>
         <form method="post" action="hallchatroom">
             <table>
                 <tr>

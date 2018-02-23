265079bfb531ebd77b8ca26ee763f40ca7ad13aa
K1rby <36076581+K1rby@users.noreply.github.com>
2018-02-23 11:46:31 +0100

    maj 1.0

--- supprimerTriangle.php
+++ supprimerTriangle.php
@@ -1,13 +1,15 @@
 <?php
 $titre = "Supprimer Triangle";
 
-
 include_once 'Config.php';
 include_once 'Classes/Point.php';
 include_once 'Classes/Polygone.php';
 include_once 'Classes/Triangle.php';
 include_once 'header.php';
 
-$this->Supprimer();
+$id=$_GET['id'];
+$tri=Triangle::getTriangle($id);
+$tri->Supprimer();
+
 header("location: index.php");
 die();
